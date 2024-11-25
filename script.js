5423c51289b02e96d1444e1adc6348ddbc0f4421
CarlosProjetosJS <carlinho937@gmail.com>
2024-11-25 07:50:55 -0300

    Implementado as funções com JS para Calcular o IMC

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-const nome = document.getElementById('nome');
-const altura = document.getElementById('altura');
-const peso = document.getElementById('peso');
+
 const calcular = document.getElementById('calcular');
-const resultado = document.getElementById('resultado');
+
 
 function imc () {
-    alert ('teste click')
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value; 
+    const resultado = document.getElementById('resultado');
+    const peso = document.getElementById('peso').value;
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let calssificacao = "";
+
+        if (valorIMC < 18.5) {
+            classificacao = 'Abaixo do peso!';
+        }else if (valorIMC < 25) {
+            classificacao = 'Com seu peso idael. Parabéns!!!';
+        }else if (valorIMC < 30) {
+            classificacao = 'Levemente acima do peso.';
+        }else if (valorIMC < 35) {
+            classificacao = 'Com obesidade grau I.';
+        }else if (valorIMC <40) {
+            classificacao = 'Com obesidade grau II.';
+        }else {
+            classificacao = 'Com obesidade grau III. CUIDADO!!!';
+        }
+
+        resultado.textContent = `${nome} Seu IMC é ${valorIMC} e você está ${classificacao}`;
+
+    }else {
+        resultado.textContent = 'Preencha todos os campos!!!'
+    }
 }
 
 calcular.addEventListener('click', imc);
